ebb31d84682a389691e55d184e4942795f42dc70
Allison Lee <allison.alee@gmail.com>
2019-10-30 17:59:15 -0400

    Partially completed

--- school.py
+++ school.py
@@ -1,1 +1,11 @@
-class School:
+class School():
+    def __init__(self, name, roster = {}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, student, grade):
+        if self.roster.get(grade):
+            self.roster[grade].append(student)
+        else:
+            self.roster[grade] = [student]
+        
