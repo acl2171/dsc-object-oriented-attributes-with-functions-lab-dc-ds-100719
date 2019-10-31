2dda72b3a8fe3924b7eda6e9fdae8e49b5c53412
Allison Lee <allison.alee@gmail.com>
2019-10-31 10:05:10 -0400

    Finished lab!

--- school.py
+++ school.py
@@ -8,4 +8,14 @@ class School():
             self.roster[grade].append(student)
         else:
             self.roster[grade] = [student]
+            
+    def grade(self, grade):
+        return self.roster.get(grade)
+    
+    def clear(self):
+        self.roster = {}
         
+    def sort_roster(self):
+        for x in self.roster.values():
+            x.sort()
+        return self.roster
